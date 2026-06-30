3911f3b220883b4a1834970ab3bd56403baf5371
skillparty <zeuz_pochoclo@hotmail.com>
2026-06-29 21:01:57 -0400

    feat(etymology): add Sección C lección C-6 (tierra 土)
    - C-6 Tierra (土): 71 caracteres
    - Sección C ahora 407 caracteres en 6 lecciones
    - Stroke data descargada para todos los caracteres nuevos
    - SW 4.67.0

--- assets/js/modules/etymology-controller.js
+++ assets/js/modules/etymology-controller.js
@@ -16,7 +16,7 @@ class EtymologyController {
     this.dataUrls = [
       "assets/data/etymology/seccion-a.json?v=2",
       "assets/data/etymology/seccion-b.json?v=24",
-      "assets/data/etymology/seccion-c.json?v=2",
+      "assets/data/etymology/seccion-c.json?v=3",
     ];
     this.strokeBaseUrl = "assets/data/etymology/strokes/";
     this.hanziWriterUrl = "assets/vendor/hanzi-writer.min.js";
